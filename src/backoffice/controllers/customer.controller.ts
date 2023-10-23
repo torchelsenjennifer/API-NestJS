af323312e11b294e5e60999807ad1371543f0e13
Jennifer <jennifertorchelsen@gmail.com>
2023-10-22 22:08:43 -0300

    ✨ ┇ Tipando a entrada e retorno de dados

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -7,35 +7,34 @@ import {
   Param,
   Body,
 } from '@nestjs/common';
+import { Customer } from '../models/customer.model';
+import { Result } from '../models/result.model';
 
 //localhost:3000/customer
 @Controller('v1/customers')
 export class CustomerController {
   @Get()
   get() {
-    return 'Obter os Clientes ';
+    return new Result(null, true, [], null);
   }
 
   @Get(':document')
   getById(@Param('document') document) {
-    return 'Obter os Clientes ' + document;
+    return new Result(null, true, {}, null);
   }
 
   @Post()
-  post(@Body() body) {
-    return body;
+  post(@Body() body: Customer) {
+    return new Result('Cliente Criado Com Sucesso!', true, body, null);
   }
 
   @Put(':document')
   put(@Param('document') document, @Body() body) {
-    return {
-      customer: document,
-      data: body,
-    };
+    return new Result('Cliente Atualizado Com Sucesso!', true, body, null);
   }
 
   @Delete(':document')
   delete(@Param('document') document) {
-    return 'Remover um  Clientes ' + document;
+    return new Result('Cliente Removido Com Sucesso!', true, null, null);
   }
 }
---
+++ src/backoffice/models/result.model.ts
@@ -0,0 +1,8 @@
+export class Result {
+  constructor(
+    public messaage: string,
+    public sucess: boolean,
+    public data: any,
+    public error: any,
+  ) {}
+}
